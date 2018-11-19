4889cd02aebfc31ba9a30dc787774896f659b68e
尾々野聖 <keks564976@gmail.com>
2018-11-19 16:48:47 +0900

    Rewrite group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  has_may :members
-  has_may :users, through: :members
+  has_many :members
+  has_many :users, through: :members
   validates :name, presence: true
 end
