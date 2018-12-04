0507e26ad2b49e8c2dee135f8bf22290c59eef0d
尾々野聖 <keks564976@gmail.com>
2018-12-04 10:28:27 +0900

    Rewrite

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ set :ssh_options, auth_methods: ['publickey'],
 set :default_ev, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"]
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
