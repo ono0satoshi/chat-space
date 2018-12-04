614e63b343478b774c345b3505abf764c64ef90b
尾々野聖 <keks564976@gmail.com>
2018-12-04 11:31:35 +0900

    Rewrite
    Can’t save to s3 problem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory = 'upload-for-reader'
-  config.asset_host = 'https://ap-northeast-1.amazonaws.com/upload-for-reader'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-for-reader'
 end
